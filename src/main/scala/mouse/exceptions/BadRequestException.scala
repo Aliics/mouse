6e1949de6a2c51d81ed61b451732e4cc62e4e46f
Aliics <Aliics@hotmail.com>
2023-10-23 19:29:54 +1300

    Scaladocs

--- src/main/scala/mouse/exceptions/BadRequestException.scala
+++ src/main/scala/mouse/exceptions/BadRequestException.scala
@@ -1,3 +1,6 @@
 package mouse.exceptions
 
+/**
+ * Throwing this exception while handling a [[mouse.Route]] will cause the response to respond with a [[mouse.StatusCode.BadRequest]].
+ */
 case class BadRequestException(message: String) extends Exception(message)
