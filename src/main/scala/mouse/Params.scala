37107b165b0b873dbf2c2861ea31ac5e79f64e11
Aliics <Aliics@hotmail.com>
2023-10-23 14:53:22 +1300

    FromParamList

--- src/main/scala/mouse/Params.scala
+++ src/main/scala/mouse/Params.scala
@@ -45,6 +45,7 @@ object Params {
   trait FromParam[P] {
     def fromParam(param: String): P
   }
+
   trait FromParamDefaults {
     implicit lazy val FromParamString: FromParam[String] = (param: String) => param
     implicit lazy val FromParamCharArray: FromParam[Array[Char]] = (param: String) => param.toCharArray
@@ -55,5 +56,16 @@ object Params {
     implicit lazy val FromParamLong: FromParam[Long] = (param: String) => param.toLong
     implicit lazy val FromParamFloat: FromParam[Float] = (param: String) => param.toFloat
     implicit lazy val FromParamDouble: FromParam[Double] = (param: String) => param.toDouble
+
+    /**
+     * Comma-separated values which can be individually parsed to a list.
+     *
+     * @tparam P Item type which is parsable using FromParam[P]
+     */
+    implicit def FromParamList[P: FromParam]: FromParam[List[P]] = (param: String) =>
+      param
+        .split(",")
+        .toList
+        .map(implicitly[FromParam[P]].fromParam(_))
   }
 }
